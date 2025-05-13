6b1cb34effca70b25fd3dfce8bedd16a2d5d01d6
FreitasIsac <166455554+FreitasIsac@users.noreply.github.com>
2025-05-13 19:17:07 -0300

    Add files via upload

---
+++ localStorage-form-login-main/assets/js/home.js
@@ -0,0 +1,8 @@
+const btnLogout = document.querySelector("#btn-logout")
+
+const logout = () => {
+    localStorage.removeItem("status")
+    window.location.href = "../../index.html"
+}
+
+btnLogout.addEventListener("click", logout)
